51687add1319571f795b59043e5c727bc73f61b0
amici <amici-ursi@users.noreply.github.com>
2016-06-02 14:21:30 -0500

    bugfixes and enhancements

--- feeds_to_ion.py
+++ feeds_to_ion.py
@@ -1,14 +1,23 @@
 import feedparser
+import praw
 
-#example texas feed: https://www.flickr.com/services/feeds/groups_pool.gne?id=47583010@N00&lang=en-us&format=atom
-#another: https://www.flickr.com/services/feeds/groups_pool.gne?id=1403197@N21&lang=en-us&format=atom
+r = praw.Reddit(user_agent="rss to reddit by /u/amici_ursi")
+r.login('username', 'password', disable_warning=True)
 
 feeds = ["https://www.flickr.com/services/feeds/groups_pool.gne?id=47583010@N00&lang=en-us&format=atom", "https://www.flickr.com/services/feeds/groups_pool.gne?id=1403197@N21&lang=en-us&format=atom"]
 
-for feed in feeds:
-    thisfeed = feedparser.parse(feed)
-    print("this feed's title is {}".format(thisfeed.feed.title))
-    for i in range(len(thisfeed.entries)):
-        print("this item is {}".format(thisfeed.entries[i].title))
-    
-    
+def main():
+    for feed in feeds:
+        thisfeed = feedparser.parse(feed)
+        print("this feed's title is {}".format(thisfeed.feed.title))
+        for i in range(len(thisfeed.entries)):
+            print("posting {}".format(thisfeed.entries[i].title))
+            try:
+                r.submit('imagesoftesting', title=thisfeed.entries[i].title, url=thisfeed.entries[i].link, captcha=None, send_replies=True, resubmit=False)
+            except praw.errors.AlreadySubmitted as e:
+                print("Already submitted. Skipping.\n")
+            except praw.errors.APIException as e:
+                print(e)
+                print("API error. Skipping.")
+
+main()
