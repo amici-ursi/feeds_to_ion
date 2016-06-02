18be3443960521bd19c27bf5a256674a53820e26
amici <amici-ursi@users.noreply.github.com>
2016-06-02 03:18:30 -0500

    more feeds

--- feeds_to_ion.py
+++ feeds_to_ion.py
@@ -1,7 +1,14 @@
 import feedparser
 
 #example texas feed: https://www.flickr.com/services/feeds/groups_pool.gne?id=47583010@N00&lang=en-us&format=atom
+#another: https://www.flickr.com/services/feeds/groups_pool.gne?id=1403197@N21&lang=en-us&format=atom
 
-onefeed = feedparser.parse('https://www.flickr.com/services/feeds/groups_pool.gne?id=47583010@N00&lang=en-us&format=atom')
+feeds = ["https://www.flickr.com/services/feeds/groups_pool.gne?id=47583010@N00&lang=en-us&format=atom", "https://www.flickr.com/services/feeds/groups_pool.gne?id=1403197@N21&lang=en-us&format=atom"]
 
-print("this feed's title is {}".format(onefeed.feed.title))
+for feed in feeds:
+    thisfeed = feedparser.parse(feed)
+    print("this feed's title is {}".format(thisfeed.feed.title))
+    for i in range(len(thisfeed.entries)):
+        print("this item is {}".format(thisfeed.entries[i].title))
+    
+    
